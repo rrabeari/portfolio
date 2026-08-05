d31c68a3461fb7f7682453440a731b6c7906cdda
Kalim Rabearison <kalimrazah2@gmail.com>
2026-08-05 12:45:14 +0000

    modified:   portfolio/angular.json

--- portfolio/src/app/features/calendar/calendar.ts
+++ portfolio/src/app/features/calendar/calendar.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [
+    FullCalendarModule
+  ],
   templateUrl: './calendar.html',
-  styleUrl: './calendar.css',
+  styleUrl: './calendar.css'
 })
-export class Calendar {}
+export class Calendar {
+
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+
+    plugins: [
+      dayGridPlugin,
+      interactionPlugin
+    ],
+
+    editable: true,
+
+    selectable: true,
+
+    events: [
+      {
+        title: 'Création Portfolio',
+        date: '2026-08-05'
+      },
+      {
+        title: 'Mise en ligne',
+        date: '2026-08-18'
+      }
+    ]
+
+  };
+
+}
