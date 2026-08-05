cb16494f2409dca474003cbe68f3217ed04d34d8
Kalim Rabearison <kalimrazah2@gmail.com>
2026-08-06 01:24:44 +0300

    modified:   portfolio/src/app/core/layout/footer/footer.css 	modified:   portfolio/src/app/core/layout/footer/footer.html 	modified:   portfolio/src/app/core/layout/footer/footer.ts 	modified:   portfolio/src/app/core/layout/navbar/navbar.css 	modified:   portfolio/src/app/core/layout/navbar/navbar.html 	modified:   portfolio/src/app/core/layout/navbar/navbar.ts 	modified:   portfolio/src/app/features/home/home.ts

--- portfolio/src/app/core/layout/navbar/navbar.ts
+++ portfolio/src/app/core/layout/navbar/navbar.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, HostListener, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+
+  // Etat menu mobile
+  isMenuOpen = signal(false);
+
+  // Etat scroll
+  isScrolled = signal(false);
+
+
+  toggleMenu(){
+
+    this.isMenuOpen.update(value => !value);
+
+  }
+
+
+  closeMenu(){
+
+    this.isMenuOpen.set(false);
+
+  }
+
+
+
+  @HostListener('window:scroll')
+  onScroll(){
+
+    this.isScrolled.set(window.scrollY > 50);
+
+  }
+
 
 }
