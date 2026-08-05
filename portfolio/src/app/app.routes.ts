369fa0a6e757712a449157052e337a4038615212
Kalim Rabearison <kalimrazah2@gmail.com>
2026-08-05 11:46:29 +0000

    modified:   portfolio/src/app/app.html     modified:   portfolio/src/app/app.routes.ts

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,75 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home')
+        .then(m => m.Home)
+  },
+
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./features/about/about')
+        .then(m => m.About)
+  },
+
+  {
+    path: 'skills',
+    loadComponent: () =>
+      import('./features/skills/skills')
+        .then(m => m.Skills)
+  },
+
+  {
+    path: 'experience',
+    loadComponent: () =>
+      import('./features/experience/experience')
+        .then(m => m.Experience)
+  },
+
+  {
+    path: 'projects',
+    loadComponent: () =>
+      import('./features/projects/projects')
+        .then(m => m.Projects)
+  },
+
+  {
+    path: 'services',
+    loadComponent: () =>
+      import('./features/services/services')
+        .then(m => m.Services)
+  },
+
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard')
+        .then(m => m.Dashboard)
+  },
+
+  {
+    path: 'calendar',
+    loadComponent: () =>
+      import('./features/calendar/calendar')
+        .then(m => m.Calendar)
+  },
+
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./features/contact/contact')
+        .then(m => m.Contact)
+  },
+
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./features/not-found/not-found')
+        .then(m => m.NotFound)
+  }
+
+];
---
+++ portfolio/src/app/core/layout/scroll-top/scroll-top.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-top',
+  imports: [],
+  templateUrl: './scroll-top.html',
+  styleUrl: './scroll-top.css',
+})
+export class ScrollTop {}
